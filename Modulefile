1456184e0776cb3808ee116f555b58cb82abb58e
Yanis Guenane <yguenane@gmail.com>
2012-08-12 15:38:34 +0200

    Handling EL 6 Series & Tomcat configuration attribute via

--- Modulefile
+++ Modulefile
@@ -1,7 +1,7 @@
 name 'yguenane-tomcat6'
-version '0.0.1'
-summary 'A Tomcat6 puppet module that installs tomcat6 on EL/Debian/Darwin platforms'
-description "This module aims to install tomcat6 on EL/Debian/Darwin platforms using their respective package managers and not by compiling the source code"
+version '0.0.2'
+summary 'A Tomcat6 puppet module that installs tomcat6 on EL/Debian platforms'
+description "This module aims to install tomcat6 on EL/Debian platforms using their respective package managers"
 license 'GPLv3'
 author 'Yanis Guenane'
 source 'https://github.com/Spredzy/puppet-tomcat6'
--- manifests/init.pp
+++ manifests/init.pp
@@ -1,12 +1,21 @@
-class tomcat6  inherits tomcat6::params {
+class tomcat6 ($config_hash = {},) inherits tomcat6::params {
 
     if $osfamily == "RedHat" {
 
-      package {'yum-priorities' :
+	  $yum_priorities = $lsbmajdistrelease ? {
+		/4|6/	=> 'yum-plugin-priorities',
+		'5'		=> 'yum-priorities',	
+	  }
+
+      package {$yum_priorities :
         ensure    => installed,
       }
 
-      Exec { path =>  ["/bin", "/usr/bin"], require => Package['yum-priorities'],}
+	  package {'java-1.6.0-openjdk' :
+		  ensure	=> installed,
+	  }
+
+      Exec { path =>  ["/bin", "/usr/bin"], require => [Package[$yum_priorities],Package['java-1.6.0-openjdk']]}
       exec {
         "rpm -Uvh $epel_url" :
           before  => Exec["rpm -Uvh $rpmforge_url"],
@@ -25,35 +34,48 @@ class tomcat6  inherits tomcat6::params {
       # Missing Dependency: /usr/bin/rebuild-security-providers is needed by package java-1.4.2-gcj-compat-1.4.2.0-40jpp.115.i386 (base)
       #
 
-      exec {'rpm -Uvh http://plone.lucidsolutions.co.nz/linux/centos/images/jpackage-utils-compat-el5-0.0.1-1.noarch.rpm' :
-        path    => ["/usr/bin", "/bin"],
-        require => Exec["wget http://jpackage.org/jpackage50.repo"],
-	      before  => Notify["start_installation"],
-        unless  => "rpm -qa | grep jpackage-utils-compat-el5-0.0.1-1";
-      }
+		exec {'rpm -Uvh http://plone.lucidsolutions.co.nz/linux/centos/images/jpackage-utils-compat-el5-0.0.1-1.noarch.rpm' :
+			path    => ["/usr/bin", "/bin"],
+			require => Exec["wget http://jpackage.org/jpackage50.repo"],
+			before  => Notify["start_installation"],
+			unless  => "rpm -qa | grep jpackage-utils-compat-el5-0.0.1-1";
+		}
 
       #
       #
 
-      $packages = ['tomcat6', 'tomcat6-admin-webapps']
-    } elsif $osfamily == "Darwin" {
-      $packages = ['tomcat6']
-    } elsif $osfamily == "Debian" {
-      $packages = ['tomcat6', 'tomcat6-admin']
     }
-
    notify {"start_installation" : }
 
-    package {[$packages] :
+   $default_params = [$tomcat6::params::tomcat_user,
+					  $tomcat6::params::tomcat_group,
+					  $tomcat6::params::java_opts,
+					  $tomcat6::params::catalina_pid,
+					  $tomcat6::params::catalina_base,
+					  $tomcat6::params::catalina_home,
+					  $tomcat6::params::jasper_home,
+					  $tomcat6::params::catalina_tmpdir,
+					  $tomcat6::params::security_manager,
+					  $tomcat6::params::shutdown_wait,
+					  $tomcat6::params::shutdown_verbose]
+	$conf_file_content = get_tomcat_conf($default_params, $config_hash)	
+
+    package {"tomcat6" :
       ensure => installed,
       require => Notify["start_installation"],
     }
+
+	file {$conf_file :
+		ensure	=> present,
+		content => $conf_file_content,
+		require	=> Package["tomcat6"],
+	}
 
      service {'tomcat6' :
       ensure      => running,
       enable      => true,
       hasrestart  => true,
       hasstatus   => true,
-      require     => Package["tomcat6"],
+	  subscribe     => File["${conf_file}"],
     }
 }
