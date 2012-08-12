8fb87cdf2d77e83b1a372f2a5553aced00355a26
Yanis Guenane <yguenane@gmail.com>
2012-08-12 16:23:37 +0200

    Clean files after passing test on puppetlinter.com

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,81 +1,75 @@
 class tomcat6 ($config_hash = {},) inherits tomcat6::params {
+  if $osfamily == "RedHat" {
 
-    if $osfamily == "RedHat" {
-
-	  $yum_priorities = $lsbmajdistrelease ? {
-		/4|6/	=> 'yum-plugin-priorities',
-		'5'		=> 'yum-priorities',	
-	  }
-
-      package {$yum_priorities :
-        ensure    => installed,
-      }
-
-	  package {'java-1.6.0-openjdk' :
-		  ensure	=> installed,
-	  }
-
-      Exec { path =>  ["/bin", "/usr/bin"], require => [Package[$yum_priorities],Package['java-1.6.0-openjdk']]}
-      exec {
-        "rpm -Uvh $epel_url" :
-          before  => Exec["rpm -Uvh $rpmforge_url"],
-          unless  => "rpm -qa | grep epel-release";
-        "rpm -Uvh $rpmforge_url" :
-          before  => Exec["wget http://jpackage.org/jpackage50.repo"],
-          unless  => "rpm -qa | grep rpmforge-release";
-        "wget http://jpackage.org/jpackage50.repo" :
-          cwd     => "/etc/yum.repos.d/",
-          unless  => "ls -la /etc/yum.repos.d/ | grep jpackage";
-      }
-
-      #
-      # The following two statement are there to correct an incorrect behvoir of CentOS 5 Series related to JPackage.
-      # TODO Check if this applies also for CentOS 6 Series
-      # Missing Dependency: /usr/bin/rebuild-security-providers is needed by package java-1.4.2-gcj-compat-1.4.2.0-40jpp.115.i386 (base)
-      #
-
-		exec {'rpm -Uvh http://plone.lucidsolutions.co.nz/linux/centos/images/jpackage-utils-compat-el5-0.0.1-1.noarch.rpm' :
-			path    => ["/usr/bin", "/bin"],
-			require => Exec["wget http://jpackage.org/jpackage50.repo"],
-			before  => Notify["start_installation"],
-			unless  => "rpm -qa | grep jpackage-utils-compat-el5-0.0.1-1";
-		}
-
-      #
-      #
-
-    }
-   notify {"start_installation" : }
-
-   $default_params = [$tomcat6::params::tomcat_user,
-					  $tomcat6::params::tomcat_group,
-					  $tomcat6::params::java_opts,
-					  $tomcat6::params::catalina_pid,
-					  $tomcat6::params::catalina_base,
-					  $tomcat6::params::catalina_home,
-					  $tomcat6::params::jasper_home,
-					  $tomcat6::params::catalina_tmpdir,
-					  $tomcat6::params::security_manager,
-					  $tomcat6::params::shutdown_wait,
-					  $tomcat6::params::shutdown_verbose]
-	$conf_file_content = get_tomcat_conf($default_params, $config_hash)	
-
-    package {"tomcat6" :
-      ensure => installed,
-      require => Notify["start_installation"],
+    $yum_priorities = $lsbmajdistrelease ? {
+      /4|6/	=> 'yum-plugin-priorities',
+      '5'		=> 'yum-priorities',
     }
 
-	file {$conf_file :
-		ensure	=> present,
-		content => $conf_file_content,
-		require	=> Package["tomcat6"],
-	}
-
-     service {'tomcat6' :
-      ensure      => running,
-      enable      => true,
-      hasrestart  => true,
-      hasstatus   => true,
-	  subscribe     => File["${conf_file}"],
+    package {$yum_priorities :
+      ensure    => installed,
     }
+
+    package {'java-1.6.0-openjdk' :
+      ensure	=> installed,
+    }
+
+    Exec { path =>  ['/bin', '/usr/bin'], require => [Package[$yum_priorities],Package['java-1.6.0-openjdk']]}
+    exec {
+      "rpm -Uvh $epel_url" :
+        before  => Exec["rpm -Uvh $rpmforge_url"],
+        unless  => 'rpm -qa | grep epel-release';
+      "rpm -Uvh $rpmforge_url" :
+        before  => Exec['wget http://jpackage.org/jpackage50.repo'],
+        unless  => 'rpm -qa | grep rpmforge-release';
+      'wget http://jpackage.org/jpackage50.repo' :
+        cwd     => '/etc/yum.repos.d/',
+        unless  => 'ls -la /etc/yum.repos.d/ | grep jpackage';
+    }
+
+    #
+    # The following two statement are there to correct an incorrect behvoir of CentOS 5 Series related to JPackage.
+    # TODO Check if this applies also for CentOS 6 Series
+    # Missing Dependency: /usr/bin/rebuild-security-providers is needed by package java-1.4.2-gcj-compat-1.4.2.0-40jpp.115.i386 (base)
+    #
+    exec {'rpm -Uvh http://plone.lucidsolutions.co.nz/linux/centos/images/jpackage-utils-compat-el5-0.0.1-1.noarch.rpm' :
+      path    => ['/usr/bin', '/bin'],
+      require => Exec['wget http://jpackage.org/jpackage50.repo'],
+      before  => Notify['start_installation'],
+      unless  => 'rpm -qa | grep jpackage-utils-compat-el5-0.0.1-1';
+    }
+  }
+
+  notify {'start_installation' : }
+  $default_params = [$tomcat6::params::tomcat_user,
+  $tomcat6::params::tomcat_group,
+  $tomcat6::params::java_opts,
+  $tomcat6::params::catalina_pid,
+  $tomcat6::params::catalina_base,
+  $tomcat6::params::catalina_home,
+  $tomcat6::params::jasper_home,
+  $tomcat6::params::catalina_tmpdir,
+  $tomcat6::params::security_manager,
+  $tomcat6::params::shutdown_wait,
+  $tomcat6::params::shutdown_verbose]
+  $conf_file_content = get_tomcat_conf($default_params, $config_hash)
+
+  package {'tomcat6' :
+    ensure => installed,
+    require => Notify['start_installation'],
+  }
+
+  file {$conf_file :
+    ensure	=> present,
+    content => $conf_file_content,
+    require	=> Package['tomcat6'],
+  }
+
+  service {'tomcat6' :
+    ensure      => running,
+    enable      => true,
+    hasrestart  => true,
+    hasstatus   => true,
+    subscribe     => File[${conf_file}],
+  }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,70 +1,65 @@
 class tomcat6::params {
-
   #
   # Distribution informations
   #
   # $architecure and $lsbmajdistrelease comes from facter
   #
-
-  $arch       = $architecture
-  $el_version = $lsbmajdistrelease
+  $arch       = $::architecture
+  $el_version = $::lsbmajdistrelease
 
   #
   # Extra package needed for installing tomcat6 informations
   #
-	$epel_release =  $lsbmajdistrelease ? {
-		'5'	=> '5-4',
-		'6'	=> '6-7',	
-	}
-	$arch_bis = $lsbmajdistrelease ? {
-		'6'	=> $arch ? {'i386' => 'i686', default => $arch,},
-		default	=> $arch,
-	}
-  $epel_url       = "http://mirror.uv.es/mirror/fedora-epel//${el_version}/${arch}/epel-release-${epel_release}.noarch.rpm"
-  $rpmforge_version   = '0.5.2-2'
-  $rpmforge_url     = "http://apt.sw.be/redhat/el${el_version}/en/${arch}/rpmforge/RPMS/rpmforge-release-${rpmforge_version}.el${el_version}.rf.${arch_bis}.rpm"
+  $epel_release =  $::lsbmajdistrelease ? {
+    '5' => '5-4',
+    '6' => '6-7',
+  }
+  $arch_bis = $::lsbmajdistrelease ? {
+    '6'     => $arch ? {'i386' => 'i686', default => $arch,},
+    default => $arch,
+  }
+  $epel_url = "http://mirror.uv.es/mirror/fedora-epel//${el_version}/${arch}/epel-release-${epel_release}.noarch.rpm"
+  $rpmforge_version = '0.5.2-2'
+  $rpmforge_url = "http://apt.sw.be/redhat/el${el_version}/en/${arch}/rpmforge/RPMS/rpmforge-release-${rpmforge_version}.el${el_version}.rf.${arch_bis}.rpm"
 
-	#
-	# Tomcat6 configuration file parameters
-	#
-	# EL : /usr/share/tomcat6/conf/tomcat6.conf
-	#
-	# Debian : /etc/default/tomcat6
-	#
-
-	case $osfamily {
-		"RedHat" : {
-			$conf_file			=	'/usr/share/tomcat6/conf/tomcat6.conf'
-			$tomcat_user		=	{'TOMCAT_USER'		=> 'tomcat'}
-			$tomcat_group		=	{'TOMCAT_GROUP'		=> 'tomcat'}
-			$java_opts			=	{'JAVA_OPTS'		=> '-Xms128m -Xmx1g'}
-			$catalina_pid		=	{'CATALINA_PID'		=> '/var/run/tomcat6.pid'}
-			$catalina_base		=	{'CATALINA_BASE'	=> '/usr/share/tomcat6'}
-			$catalina_home		=	{'CATALINA_HOME'	=> '/usr/share/tomcat6'}
-			$jasper_home		=	{'JASPER_HOME'		=> '/usr/share/tomcat6'}
-			$catalina_tmpdir	=	{'CATALINA_TMPDIR'	=> '/var/cache/tomcat6/temp'}
-			$security_manager	=	{'SECURITY_MANAGER'	=>	'false'}
-			$shutdown_wait		=	{'SHUTDOWN_WAIT'	=>	'30'}
-			$shutdown_verbose	=	{'SHUTDOWN_VERBOSE'	=>	'false'}
-		}
-		"Debian" : {
-			$conf_file			=	'/etc/default/tomcat6'
-			$tomcat_user		=	{'TOMCAT6_USER'		=> 'tomcat6'}
-			$tomcat_group		=	{'TOMCAT6_GROUP'	=> 'tomcat6'}
-			$java_opts			=	{'JAVA_OPTS'		=> '-Xms128m -Xmx1g'}
-			$catalina_pid		=	{'CATALINA_PID'		=> '/var/run/tomcat6.pid'}
-			$catalina_base		=	{'CATALINA_BASE'	=> '/var/lib/tomcat6'}
-			$catalina_home		=	{'CATALINA_HOME'	=> '/usr/share/tomcat6'}
-			$jasper_home		=	{'JASPER_HOME'		=> '/usr/share/tomcat6'}
-			$catalina_tmpdir	=	{'CATALINA_TMPDIR'	=> '/var/cache/tomcat6/temp'}
-			$security_manager	=	{'SECURITY_MANAGER'	=>	'false'}
-			$shutdown_wait		=	{'SHUTDOWN_WAIT'	=>	'30'}
-			$shutdown_verbose	=	{'SHUTDOWN_VERBOSE'	=>	'false'}
-		}
-		default : {
-			fail("Unsupported osfamily: ${::osfamily} operatingsystem: ${::operatingsystem}, module ${module_name} only support osfamily RedHat Debian")
-		}
-	}
-
-
+  #
+  # Tomcat6 configuration file parameters
+  #
+  # EL : /usr/share/tomcat6/conf/tomcat6.conf
+  #
+  # Debian : /etc/default/tomcat6
+  #
+  case $::osfamily {
+    'RedHat' : {
+      $conf_file        = '/usr/share/tomcat6/conf/tomcat6.conf'
+      $tomcat_user      = {'TOMCAT_USER'      => 'tomcat'}
+      $tomcat_group     = {'TOMCAT_GROUP'     => 'tomcat'}
+      $java_opts        = {'JAVA_OPTS'        => '-Xms128m -Xmx1g'}
+      $catalina_pid     = {'CATALINA_PID'     => '/var/run/tomcat6.pid'}
+      $catalina_base    = {'CATALINA_BASE'    => '/usr/share/tomcat6'}
+      $catalina_home    = {'CATALINA_HOME'    => '/usr/share/tomcat6'}
+      $jasper_home      = {'JASPER_HOME'      => '/usr/share/tomcat6'}
+      $catalina_tmpdir  = {'CATALINA_TMPDIR'  => '/var/cache/tomcat6/temp'}
+      $security_manager = {'SECURITY_MANAGER' => false}
+      $shutdown_wait    = {'SHUTDOWN_WAIT'    => '30'}
+      $shutdown_verbose = {'SHUTDOWN_VERBOSE' => false}
+    }
+    'Debian' : {
+      $conf_file        = '/etc/default/tomcat6'
+      $tomcat_user      = {'TOMCAT6_USER'     => 'tomcat6'}
+      $tomcat_group     = {'TOMCAT6_GROUP'    => 'tomcat6'}
+      $java_opts        = {'JAVA_OPTS'        => '-Xms128m -Xmx1g'}
+      $catalina_pid     = {'CATALINA_PID'     => '/var/run/tomcat6.pid'}
+      $catalina_base    = {'CATALINA_BASE'    => '/var/lib/tomcat6'}
+      $catalina_home    = {'CATALINA_HOME'    => '/usr/share/tomcat6'}
+      $jasper_home      = {'JASPER_HOME'      => '/usr/share/tomcat6'}
+      $catalina_tmpdir  = {'CATALINA_TMPDIR'  => '/var/cache/tomcat6/temp'}
+      $security_manager = {'SECURITY_MANAGER' => false}
+      $shutdown_wait    = {'SHUTDOWN_WAIT'    => '30'}
+      $shutdown_verbose = {'SHUTDOWN_VERBOSE' => false}
+    }
+    default : {
+      fail("Unsupported osfamily: ${::osfamily} operatingsystem: ${::operatingsystem}, module ${module_name} only support osfamily RedHat Debian")
+    }
+  }
 }
