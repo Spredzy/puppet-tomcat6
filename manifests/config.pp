d4b0bfb0235b4f8256adf18fe24a430e0ed42f0e
Yanis Guenane <yguenane@gmail.com>
2012-09-13 18:52:13 +0200

    It checks every 3 second if tomcat is started in order to restart the service

--- manifests/config.pp
+++ manifests/config.pp
@@ -14,18 +14,26 @@ class tomcat6::config ($config_hash = {}) inherits tomcat6::params {
   $conf_file_content = get_tomcat_conf($default_params, $config_hash)
 
   file {$conf_file :
-  	ensure  => present,
-  	content => $conf_file_content,
-	notify  => Exec['tomcat6-restart'],
+    ensure  => present,
+    content => $conf_file_content,
+    notify  => Exec["curl --silent --show-error -I http://localhost:8080 | grep 'Coyote'"],
+  }
+
+  # This kind of suck but in order to avoid race condition on tomcat restart it is necessary
+  # I did not find any other way to bypass it, if you have an idea hit me up on GitHub
+  exec {"curl --silent --show-error -I http://localhost:8080 | grep 'Coyote'" :
+    path        => '/usr/sbin:/sbin:/usr/bin:/bin',
+    refreshonly => true,
+    tries       => 10,
+    try_sleep   => 3,
+    notify      => Exec['tomcat6-restart'],
   }
 
   exec {'tomcat6-restart':
-	command     => "service tomcat6 restart",
-	refreshonly => true,
-	logoutput   => true,
-	path        => '/usr/sbin:/sbin:/usr/bin:/bin',
-	tries       => 3,
-	try_sleep   => 3,
+    command     => "service tomcat6 restart",
+    refreshonly => true,
+    logoutput   => true,
+    path        => '/usr/sbin:/sbin:/usr/bin:/bin',
   }
 
 }
